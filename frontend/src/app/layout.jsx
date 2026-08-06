a23fae0c4a5a05ac565f128d412f4b061e6be24b
ALAN HENRIQUE FREITAS GOMES <25170119@educ123.sp.senai.br>
2026-08-06 13:26:16 -0300

    FIX: import

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "@/components/Header";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +23,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      
+      <body>
+        <Header/>
+        {children}
+        
+        </body>
     </html>
   );
 }
